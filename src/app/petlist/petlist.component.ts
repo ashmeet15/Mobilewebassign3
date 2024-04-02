44e5155b4a248e5d480d1499cdc89846a17f6b9f
Ashmeet Kaur <kau15556@sheridancollege.ca>
2024-04-02 16:55:58 -0400

    component created

---
+++ src/app/petlist/petlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-petlist',
+  templateUrl: './petlist.component.html',
+  styleUrl: './petlist.component.css'
+})
+export class PetlistComponent {
+
+}
